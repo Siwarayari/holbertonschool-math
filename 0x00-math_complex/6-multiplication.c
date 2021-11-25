b4ee670b55486e642a35c3c53339c0ce698cd9aa
siwar9910 <siwarayari1999@gmail.com>
2021-11-25 10:32:24 -0800

    task6

--- 0x00-math_complex/6-multiplication.c
+++ 0x00-math_complex/6-multiplication.c
@@ -11,14 +11,4 @@ void multiplication(complex c1, complex c2, complex *c3)
 {
 	c3->re = ((c1.re * c2.re) - (c1.im * c2.im));
 	c3->im = ((c1.re * c2.im) + (c1.im * c2.re));
-}lication - performs multiplication of complex numbers
-* @c1: First complex
-* @c2: Second complex
-* @c3: Result complex
- */
-
-void multiplication(complex c1, complex c2, complex *c3)
-{
-  c3->re = ((c1.re * c2.re) - (c1.im * c2.im));
-  c3->im = ((c1.re * c2.im) + (c1.im * c2.re));
 }
