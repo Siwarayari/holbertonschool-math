89f3b29ef408c750e4e016e4e6e5cef2cf3ee887
siwar9910 <siwarayari1999@gmail.com>
2021-11-26 08:46:15 -0800

    task1

--- 0x01-math_sequence/1-fibonacci.c
+++ 0x01-math_sequence/1-fibonacci.c
@@ -1,86 +1,67 @@
 #include "fibonacci.h"
 
 /**
- * reverse_list - reverses a linked list.
- * @head: Pointer to list's head pointer
- * Return: Pointer to first node of reversed list
- */
-
-t_cell *reverse_list(t_cell **head)
-{
-	t_cell *aux, *tmp;
-
-	aux = *head;
-	tmp = NULL;
-	while (*head)
-	{
-		(*head) = (*head)->next;
-		aux->next = tmp;
-		tmp = aux;
-		aux = *head;
-	}
-	(*head) = tmp;
-	return (*head);
-}
-
-/**
- * Create_Fib - Creates a fibonacci sequence
- * @num1: First term of fibonacci sequence
- * @num2: Second term of Fibonacci sequence
- * Return: Pointer to sequence
- */
-
-t_cell *Create_Fib(double num1, double num2)
-{
-	t_cell *ret;
-
-	ret = (t_cell *)malloc(sizeof(t_cell));
-	if (ret == NULL)
-		return (NULL);
-	ret->elt = num1 + num2;
-	if (fabs((ret->elt / num2) - (num2 / num1)) <= 10E-8)
-		ret->next = NULL;
-	else
-		ret->next = Create_Fib(num2, ret->elt);
-	return (ret);
-}
-
-/**
- * Fibonnaci - Fibonacci sequence until having a geometric behavior
- * Return: Pointer to F1
- */
+* Fibonnaci - fibonacci
+* Return: void
+*/
 
 t_cell *Fibonnaci()
 {
-	t_cell *f1, *f2;
+double goldn = 1.618034;
+double togold = 2;
+int mflag = 1;
+double diff = 0.00000005, oper;
+t_cell *prevnode, *preprevnode, *tempnode, *fibon;
 
-	f1 = (t_cell *)malloc(sizeof(t_cell));
-	if (f1 == NULL)
-		return (NULL);
-	f2 = (t_cell *)malloc(sizeof(t_cell));
-	if (f2 == NULL)
-		return (NULL);
-	f1->elt = (double)1;
-	f1->next = f2;
-	f2->elt = (double)1;
-	f2->next = Create_Fib(f1->elt, f2->elt);
-	f1 = reverse_list(&f1);
-	return (f1);
+fibon = malloc(sizeof(t_cell));
+	if (fibon == NULL)
+		free(fibon);
+fibon->elt = 1;
+fibon->next = NULL;
+preprevnode = fibon;
+prevnode = fibon;
+tempnode = malloc(sizeof(t_cell));
+	if (tempnode == NULL)
+		free(tempnode);
+tempnode->elt = 1;
+tempnode->next = preprevnode;
+preprevnode = tempnode;
+	while (mflag)
+	{
+		tempnode = malloc(sizeof(t_cell));
+			if (tempnode == NULL)
+				free(tempnode);
+		tempnode->elt = preprevnode->elt + prevnode->elt;
+		tempnode->next = preprevnode;
+		prevnode = preprevnode;
+		preprevnode = tempnode;
+		togold = (double) preprevnode->elt / prevnode->elt;
+			if (togold > goldn)
+				oper = togold - goldn;
+			else
+				oper = goldn - togold;
+
+			if (oper <= diff)
+				mflag = 0;
+	}
+return (preprevnode);
 }
 
 /**
- * gold_number - calcs the gold number
- * @head: Fibonacci's sequence head pointer
- * Return: Value of gold number
- */
-
+* gold_number - calculate gold number from fibonacci numbers
+* @head: list
+* Return: void
+*/
 double gold_number(t_cell *head)
 {
-	double gold, aux;
-
-	gold =  (double)(head->elt) / (double)((head->next)->elt);
-	aux = (double)((head->next)->elt) / (double)(((head->next)->next)->elt);
-	if (fabs((aux / gold) - 1) >= 10E-7)
-		gold = gold_number(head->next);
-	return (gold);
+double thediv;
+t_cell *a;
+t_cell *b;
+/* receive a SLL divide the last number with the previous */
+a = head;
+head = head->next;
+b = head;
+thediv = (double) a->elt / b->elt;
+/* Thats it */
+return (thediv);
 }
